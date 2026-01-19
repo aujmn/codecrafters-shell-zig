e4c2fa2ada9be6ac1a659fcd693f9d41bcd075bb
Jun <jun@jun.local>
2026-01-19 21:15:22 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -4,13 +4,18 @@ var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
+    const keywords = .{ .{"exit"}, .{"echo"}, .{"type"} }; // todo: improve list of tuple construction
+    // var keywords_buffer: [2048]u8 = undefined;
+    // var keywords_allocator = std.heap.FixedBufferAllocator.init(&keywords_buffer);
+    const keywords_map = std.static_string_map.StaticStringMap(void).initComptime(keywords);
+
+    var input_buffer: [2048]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().readerStreaming(&input_buffer);
+    var stdin = &stdin_reader.interface;
+
     exit: while (true) {
         try stdout.print("$ ", .{});
-
-        var input_buffer: [2048]u8 = undefined;
-        var stdin_reader = std.fs.File.stdin().readerStreaming(&input_buffer);
-        var stdin = &stdin_reader.interface;
-        const input = try stdin.takeDelimiter('\n') orelse return; // fixme: return on empty?
+        const input = try stdin.takeDelimiter('\n') orelse return; // fixme: what to do on empty input?
         const trimmed_input = std.mem.trim(u8, input, "\n");
 
         if (std.mem.eql(u8, trimmed_input, "exit")) {
@@ -19,6 +24,16 @@ pub fn main() !void {
 
         if (std.mem.startsWith(u8, trimmed_input, "echo ")) {
             try stdout.print("{s}\n", .{trimmed_input[5..]});
+            continue :exit;
+        }
+
+        if (std.mem.startsWith(u8, trimmed_input, "type ")) {
+            const command = trimmed_input[5..];
+            if (keywords_map.has(command)) {
+                try stdout.print("{s} is a shell builtin\n", .{command});
+            } else {
+                try stdout.print("{s}: not found\n", .{command});
+            }
             continue :exit;
         }
 
