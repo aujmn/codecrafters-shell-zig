75ed0d1ef337b37cabaed4077b56f045633e9547
Jun <jun@jun.local>
2026-01-19 20:15:26 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -5,4 +5,11 @@ const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
     try stdout.print("$ ", .{});
+
+    var input_buffer: [2048]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().readerStreaming(&input_buffer);
+    var stdin = &stdin_reader.interface;
+    const input = try stdin.takeDelimiter('\n') orelse return; // fixme: return on empty?
+    const trimmed_input = std.mem.trim(u8, input, "\n");
+    try stdout.print("{s}: command not found\n", .{trimmed_input});
 }
