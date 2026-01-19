6f010107d9addad1a5c115fa2a51c801784a871b
Jun <jun@jun.local>
2026-01-19 20:32:19 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -12,9 +12,16 @@ pub fn main() !void {
         var stdin = &stdin_reader.interface;
         const input = try stdin.takeDelimiter('\n') orelse return; // fixme: return on empty?
         const trimmed_input = std.mem.trim(u8, input, "\n");
+
         if (std.mem.eql(u8, trimmed_input, "exit")) {
             break :exit;
         }
+
+        if (std.mem.startsWith(u8, trimmed_input, "echo ")) {
+            try stdout.print("{s}\n", .{trimmed_input[5..]});
+            continue :exit;
+        }
+
         try stdout.print("{s}: command not found\n", .{trimmed_input});
     }
 }
