7bb6c604ade52fc915f528b699b57e77e9feff73
Jun <125691637+aujmn@users.noreply.github.com>
2026-01-30 13:58:09 +0800

    Run a program

--- src/main.zig
+++ src/main.zig
@@ -16,38 +16,53 @@ pub fn main() !void {
     defer arena.deinit();
     const alloc = arena.allocator();
 
-    exit: while (true) {
+    while (true) {
         try stdout.print("$ ", .{});
         const input = try stdin.takeDelimiter('\n') orelse return; // terminate on EOF
-        if (input.len == 0) {
-            continue :exit;
+        // parse arguments and skip over whitespace
+        var it = std.mem.splitAny(u8, input, " \t");
+        var arg_list = try std.ArrayList([]const u8).initCapacity(alloc, 64);
+        while (it.next()) |arg| {
+            if (!std.mem.eql(u8, arg, "")) {
+                try arg_list.append(alloc, arg);
+            }
         }
-
-        if (std.mem.eql(u8, input, "exit")) {
-            break :exit;
+        const args = try arg_list.toOwnedSlice(alloc);
+        defer alloc.free(args);
+        if (args.len == 0) {
+            continue;
         }
+        const command = args[0];
 
-        if (std.mem.startsWith(u8, input, "echo ")) {
-            try stdout.print("{s}\n", .{input[5..]});
-            continue :exit;
-        }
-
-        if (std.mem.startsWith(u8, input, "type ")) {
-            const command = input[5..];
-            if (builtin_keywords_set.has(command)) {
-                try stdout.print("{s} is a shell builtin\n", .{command});
+        if (std.mem.eql(u8, command, "exit")) {
+            break;
+        } else if (std.mem.eql(u8, command, "echo")) {
+            for (args[1 .. args.len - 1]) |arg| {
+                try stdout.print("{s} ", .{arg});
+            }
+            try stdout.print("{s}\n", .{args[args.len - 1]});
+        } else if (std.mem.eql(u8, command, "type")) {
+            std.debug.assert(args.len == 2);
+            const arg = args[1];
+            if (builtin_keywords_set.has(arg)) {
+                try stdout.print("{s} is a shell builtin\n", .{arg});
             } else {
-                const result = try path.which(alloc, command); // todo: handle error
+                const result = try path.which(alloc, arg);
                 if (result) |full_path| {
-                    try stdout.print("{s} is {s}\n", .{ command, full_path });
+                    try stdout.print("{s} is {s}\n", .{ arg, full_path });
+                    alloc.free(full_path);
                 } else {
-                    try stdout.print("{s}: not found\n", .{command});
+                    try stdout.print("{s}: not found\n", .{arg});
                 }
             }
-
-            continue :exit;
+        } else {
+            const result = try path.which(alloc, command);
+            if (result) |_| {
+                var child = std.process.Child.init(args, alloc);
+                _ = try child.spawnAndWait();
+            } else {
+                try stdout.print("{s}: command not found\n", .{input});
+            }
         }
-
-        try stdout.print("{s}: command not found\n", .{input});
     }
 }
--- src/path.zig
+++ src/path.zig
@@ -1,20 +1,26 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-pub fn which(alloc: std.mem.Allocator, exe: []const u8) !?[]const u8 {
+pub fn which(alloc: std.mem.Allocator, command: []const u8) !?[]const u8 {
     if (builtin.os.tag == .windows) {
         return error.UnsupportedPlatform;
     }
     const PATH = try std.process.getEnvVarOwned(alloc, "PATH");
+    defer alloc.free(PATH);
     var iterator = std.mem.tokenizeScalar(u8, PATH, std.fs.path.delimiter);
     while (iterator.next()) |path| {
-        // todo: follow symlinks?
-        // todo: what if PATH contains relative paths?
-        var dir = std.fs.openDirAbsolute(path, .{}) catch continue;
+        var abs_path: []const u8 = undefined;
+        defer alloc.free(abs_path);
+        if (!std.fs.path.isAbsolute(path)) {
+            abs_path = try std.fs.cwd().realpathAlloc(alloc, path);
+        } else {
+            abs_path = path;
+        }
+        var dir = std.fs.openDirAbsolute(abs_path, .{}) catch continue;
         defer dir.close();
-        const stat = dir.statFile(exe) catch continue;
+        const stat = dir.statFile(command) catch continue;
         if (fileIsExecutable(stat)) {
-            return try constructFullPath(alloc, path, exe);
+            return try constructFullPath(alloc, abs_path, command);
         }
     }
     return null;
@@ -23,14 +29,10 @@ pub fn which(alloc: std.mem.Allocator, exe: []const u8) !?[]const u8 {
 fn fileIsExecutable(stat: std.fs.File.Stat) bool {
     const mode = stat.mode;
     const exe_permission = mode & 0o111;
-    if (exe_permission != 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return exe_permission != 0;
 }
 
-fn constructFullPath(alloc: std.mem.Allocator, path: []const u8, name: []const u8) ![]u8 {
+fn constructFullPath(alloc: std.mem.Allocator, path: []const u8, command: []const u8) ![]u8 {
     const sep = [1]u8{std.fs.path.sep};
-    return try std.mem.concat(alloc, u8, &[_][]const u8{ path, &sep, name });
+    return try std.mem.concat(alloc, u8, &[_][]const u8{ path, &sep, command });
 }
